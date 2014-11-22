95a8e21273e5f69e3f8d2f2b60a7f6940a926eb1
Jake Tober <jake@macadamian.com>
2014-11-22 11:31:02 -0500

    Simple Message Passing working via Socket.IO

--- WebGame/app.js
+++ WebGame/app.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 80);
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+	    console.log('a user disconnected');
+	});
+});
+
+var count =0;
+setInterval(function(){
+	count++;
+   io.emit('count', {'value':count});
+   console.log(count);
+}, 1000);
+
+http.listen(process.env.PORT || 80);
