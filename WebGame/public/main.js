5eaaaf907ea69e2e42528f94fc40ed3df1a6ec19
Jake Tober <jake@macadamian.com>
2014-11-22 15:24:10 -0500

    Switch to Flow-->Acceleration, make collisions non-fatal

--- WebGame/public/main.js
+++ WebGame/public/main.js
@@ -1,8 +1,17 @@
-var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(1024, 600, Phaser.AUTO, 'gameDiv');
+
+var FLOW_TO_ACCEL_MULTIPLIER = 30;
+var WORLD_WIDTH = 1024;
+var WORLD_HEIGHT = 800;
 
 function capVelocity(value)
 { 
-	return Math.min(400, Math.max(-400, value));
+	return Math.min(300, Math.max(-300, value));
+}
+
+function capAcceleration(value)
+{
+	return Math.min(0, Math.max(-2000, value));	
 }
 
 var world;
@@ -25,12 +34,12 @@ var mainState = {
         world.pipes = game.add.group();
         world.pipes.enableBody = true;
         world.pipes.createMultiple(20, 'pipe');  
-        world.timer = world.game.time.events.loop(2500, world.addRowOfPipes, world);           
+        world.timer = world.game.time.events.loop(3500, world.addRowOfPipes, world);           
 		
 		
         world.bird = world.game.add.sprite(100, 245, 'bird');
         game.physics.arcade.enable(world.bird);
-        world.bird.body.gravity.y = 1000; 
+        world.bird.body.gravity.y = 500; 
 		world.bird.body.width = 30;
 		world.bird.body.height = 30;
 		world.bird.body.offset.x = 10;
@@ -50,14 +59,19 @@ var mainState = {
     },
 
 	update: function() {
-        if (world.bird.inWorld == false)
-            world.restartGame(); 
+        //if (world.bird.inWorld == false)
+        //    world.restartGame(); 
+		
+		if (world.bird.y < 0){world.bird.y = WORLD_HEIGHT-50;}
+		if (world.bird.y > WORLD_HEIGHT - 50){world.bird.y = 0;}
 
-        game.physics.arcade.overlap(world.bird, world.pipes, world.hitPipe, null, world); 
+        game.physics.arcade.overlap(world.bird, world.pipes, world.hitCoin, null, world); 
 		
         // Slowly rotate the bird downward, up to a certain point.
 		var dy = world.bird.body.velocity.y;
 		var dy = capVelocity(dy);
+		world.bird.body.velocity.y = dy;
+		
 		var dy = dy / 400;
 		
 		var targetAngle = Math.round(dy * 75);
@@ -99,19 +113,28 @@ var mainState = {
 	{
 		if (howMuch != 0  && world.bird.alive)
 		{
-			var currentVelocity = capVelocity(world.bird.body.velocity.y);
-			var newVelocity = capVelocity(currentVelocity - howMuch);
-			world.bird.body.velocity.y = newVelocity;
+			var currentAcceleration = capAcceleration(world.bird.body.acceleration.y);
+			var newAcceleration = capAcceleration(0 - (howMuch*FLOW_TO_ACCEL_MULTIPLIER));
+			world.bird.body.acceleration.y = newAcceleration;
 		}
 	},
+	
+	hitCoin: function(bird, coin) {
+	    world.score += 1;
+        world.labelScore.text = world.score; 
+		
+		coin.body.x = -100;
+		coin.alive = false;
+    },
 
-    hitPipe: function() {
+    hitPipe: function(bird, pipe) {
         // If the bird has already hit a pipe, we have nothing to do
         if (world.bird.alive == false)
             return;
             
         // Set the alive property of the bird to false
         world.bird.alive = false;
+		world.bird.body.acceleration.y = 0;
 
         // Prevent new pipes from appearing
         world.game.time.events.remove(world.timer);
@@ -130,8 +153,8 @@ var mainState = {
         var pipe = world.pipes.getFirstDead();
 
         pipe.reset(x, y);
-        pipe.body.velocity.x = -200;
-		pipe.body.width = 1;
+        pipe.body.velocity.x = -100;
+		//pipe.body.width = 1;
         pipe.checkWorldBounds = true;
         pipe.outOfBoundsKill = true;
     },
@@ -139,12 +162,9 @@ var mainState = {
     addRowOfPipes: function() {
         var hole = Math.floor(Math.random()*5)+1;
         
-        for (var i = 0; i < 8; i++)
-            if (i != hole && i != hole +1) 
-                world.addOnePipe(400, i*60+10);   
-    
-        world.score += 1;
-        world.labelScore.text = world.score;  
+        for (var i = 0; i < WORLD_HEIGHT / 60; i++)
+            if (i == hole && i != hole +1) 
+                world.addOnePipe(WORLD_WIDTH, i*60+10);    
     },
 };
 
