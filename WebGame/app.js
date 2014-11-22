e6aee72e821467f61a88e82324795f3b2ee0311b
Jake Tober <jake@macadamian.com>
2014-11-22 11:39:19 -0500

    Added Johnny-Five dependencies and LED demo (not tested)

--- WebGame/app.js
+++ WebGame/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var five = require("johnny-five");
+var board = new five.Board();
 
 app.use(express.static(__dirname + '/public'));
 
@@ -18,5 +20,10 @@ setInterval(function(){
    io.emit('count', {'value':count});
    console.log(count);
 }, 1000);
+
+board.on('ready', function(){
+    var led = new five.Led(13);
+	led.strobe();
+});
 
 http.listen(process.env.PORT || 80);
